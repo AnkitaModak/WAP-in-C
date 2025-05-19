26ef7fc3890573a330a7f02550396a6961030eac
Ankita <ankita180206@gmail.com>
2025-05-19 20:58:37 +0530

    Update printtable.c

--- printtable.c
+++ printtable.c
@@ -4,10 +4,11 @@ int main() {
     int num,table;
     printf("till which digit you want to print the table?");
     scanf("%d" , &num);
-    for(int i = 1 ;  i <= 10; i++){
-        for(int j = 1; j <= num; j++){
+    for(int i = 1 ;  i <= num; i++){
+        printf("Table of %d: \n" ,i );
+        for(int j = 1; j <= 10; j++){
             table = j * i;
-            printf("%d " , table);
+            printf("%d\t" , table);
         }
         printf("\n");
     }
