11d7c9afeea20f4195745d82028175092bb556c5
Ankita <ankita180206@gmail.com>
2025-06-11 00:56:14 +0530

    Update print01pattern.c

--- print01pattern.c
+++ print01pattern.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int i , j ;
+    int i , j,num,start ;
     for(i = 1 ; i <= 3 ; i++){
-    for(j =3 ; j>=i ; j--){
+        for(j =3 ; j>i ; j--){
             printf(" ");
-    }
-        for(j = 0 ; j < (2*i -1 ); j ++){
-            printf("%d" , j %2);
+    }   
+        start = (i %2 ==1)?0:1;
+        num = start;
+        for(j = 1 ; j <= (2*i -1 ); j ++){
+            printf("%d" ,  num);
+            num = 1 - num;
         }
         printf("\n");
 }
