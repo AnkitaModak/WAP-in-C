a621f9a2179e55993fa1e243ea55df7d966bda64
Ankita <ankita180206@gmail.com>
2025-08-07 22:20:32 +0530

    Update sortde.c

--- Array/sortde.c
+++ Array/sortde.c
@@ -2,7 +2,6 @@
 
 //this function helps in printing the array👇
 void print_array(int arr[100],int n){
-    printf("here is your beautiful array:\n");
     for(int i = 0;i<n ;i++){
         printf("%d ",arr[i]);
     }
@@ -14,6 +13,7 @@ void enter_array(int arr[100],int n){
         printf("enter the element of the array you wish to sort:");
         scanf("%d",&arr[i]);
     }
+    printf("here is your beautiful array:\n");
     print_array(arr,n);//prints the array as it is
 }
 //sorting in ascending order👇
@@ -21,8 +21,8 @@ void sortas(int arr[100],int n){
     int i,temp;
     enter_array(arr,n);
     printf("here is your sorted array:\n");
-    for(i = 0; i>n-1;i++){
-        if(arr[i]>arr[i+1]){
+    for(i = 0; i<n-1;i++){
+        if(arr[i]<arr[i+1]){
             //swap
             temp = arr[i+1];
             arr[i+1] = arr[i];
